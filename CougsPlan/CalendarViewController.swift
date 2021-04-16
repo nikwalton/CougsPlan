d8830dbe8013775f38d3f0bf7f139872b6dc88f3
Nikolaus Walton <nikawalton2@gmail.com>
2021-04-16 02:40:54 -0700

    Implemented picking dates at console level

--- CougsPlan/CalendarViewController.swift
+++ CougsPlan/CalendarViewController.swift
@@ -6,16 +6,25 @@
 //
 
 import UIKit
+import FSCalendar
 
-class CalendarViewController: UIViewController {
+class CalendarViewController: UIViewController, FSCalendarDelegate {
 
+    @IBOutlet var calendar: FSCalendar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        calendar.delegate = self
     }
-    
-
+        
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        let format = DateFormatter()
+        format.dateFormat = "MM-dd-YYYY"
+        let string = format.string(from: date)
+        print(string)
+    }
     /*
     // MARK: - Navigation
 
