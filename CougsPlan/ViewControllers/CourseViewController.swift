1b77b5c4d44ca974d3948b7c0e20a20383b0ec10
Nikolaus Walton <nikawalton2@gmail.com>
2021-05-05 02:38:08 -0700

    Courses page now only shows the courses for the specified day

--- CougsPlan/ViewControllers/CourseViewController.swift
+++ CougsPlan/ViewControllers/CourseViewController.swift
@@ -13,7 +13,7 @@ class CourseViewController: UIViewController, UITableViewDataSource {
 
     @IBOutlet weak var CourseTable: UITableView!
     var courses: [Course] = []
-    
+    var filteredCourses: [Course] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -46,6 +46,79 @@ class CourseViewController: UIViewController, UITableViewDataSource {
         }
     }
     
+    
+    @IBAction func DateChanged(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            print("Monday")
+            
+            //see if the days string has the M character
+            let set = CharacterSet(charactersIn: "M")
+            
+            filteredCourses.removeAll()
+            for course in courses {
+                let dateString = course.days
+                if dateString.rangeOfCharacter(from: set) != nil {
+                    filteredCourses.append(course)
+                }
+            }
+            CourseTable.reloadData()
+            
+        }
+        else if sender.selectedSegmentIndex == 1 {
+            print("Tuesday")
+            //for Tu and Th we have to look for the substring specifically
+            filteredCourses.removeAll()
+            for course in courses {
+                let dateString = course.days
+                if dateString.contains("Tu") {
+                    filteredCourses.append(course)
+                }
+            }
+            CourseTable.reloadData()
+        }
+        else if sender.selectedSegmentIndex == 2 {
+            print("Wednesday")
+            //see if W character is in the days string
+            let set = CharacterSet(charactersIn: "W")
+            
+            filteredCourses.removeAll()
+            for course in courses {
+                let dateString = course.days
+                if dateString.rangeOfCharacter(from: set) != nil {
+                    filteredCourses.append(course)
+                }
+            }
+            CourseTable.reloadData()
+        }
+        else if sender.selectedSegmentIndex == 3 {
+            print("Thursday")
+            //look for the Th substring specifically
+            filteredCourses.removeAll()
+            for course in courses {
+                let dateString = course.days
+                if dateString.contains("Th") {
+                    filteredCourses.append(course)
+                }
+            }
+            CourseTable.reloadData()
+        }
+        else if sender.selectedSegmentIndex == 4 {
+            print("Friday")
+            //see if the F character is in the days string
+            let set = CharacterSet(charactersIn: "F")
+            
+            filteredCourses.removeAll()
+            for course in courses {
+                let dateString = course.days
+                if dateString.rangeOfCharacter(from: set) != nil {
+                    filteredCourses.append(course)
+                }
+            }
+            CourseTable.reloadData()
+        }
+    }
+    
+    
     @IBAction func backCourseUnwind(unwindSegue: UIStoryboardSegue) {
         print("back to courses")
     }
@@ -61,7 +134,7 @@ class CourseViewController: UIViewController, UITableViewDataSource {
             CourseDays.append("M")
         }
         if vc.TuesdaySlider.isOn {
-            CourseDays.append("T")
+            CourseDays.append("Tu")
         }
         if vc.WednesdaySlider.isOn {
             CourseDays.append("W")
@@ -105,12 +178,12 @@ class CourseViewController: UIViewController, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return courses.count
+        return filteredCourses.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = CourseTable.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        let course = courses[indexPath.row]
+        let course = filteredCourses[indexPath.row]
         cell.textLabel?.text = course.name
         cell.detailTextLabel?.text = course.location
         return cell
